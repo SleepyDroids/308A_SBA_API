e2faa9447cef272d057ff4f770d25c3714c4b3fe
SleepyDroids <jacquelinelwrites@gmail.com>
2025-09-29 19:18:42 -0400

    adding comments

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ async function initialPanel() {
     const data = await response.json();
     // deck targets the array inside my data so I can loop through it more easily
     const deck = data.cards;
-
+    console.log(data);
     // for each card, do the following (78 cards total - indexed from 0 - 77)
     deck.forEach((card, index) => {
       const newDiv = document.createElement("div");
@@ -44,7 +44,7 @@ async function initialPanel() {
       newDiv.className = "card";
       // adding their index as an id in case I need to use that for later
       newDiv.id = index;
-      // for accessibility 
+      // for accessibility, repurposing divs to act like list items 
       newDiv.role = "listitem";
       // testing data retrieval is successful
       newDiv.textContent = card.name;
@@ -68,12 +68,17 @@ console.log(cardsPanel);
 const allCards = document.getElementsByClassName("card");
 console.log(allCards) // returns HTML collection
 
+// live searches based on user input
 searchBar.addEventListener("input", liveSearch)
 
 function liveSearch() {
+  // the user's input
     const query = this.value.toLowerCase(); 
+   // for each card inside of the HTML collection (deck) 
     for (let card of allCards) {
+      // the text content of each card
         const text = card.textContent.toLowerCase();
+        // if it includes the user search, show it : if not, don't 
         card.style.display = text.includes(query) ? "block" : "none"
     }
 }
