e4390de235a63a41c674f72d7c24697230409ea9
SleepyDroids <jacquelinelwrites@gmail.com>
2025-09-29 21:57:34 -0400

    working on filter for card types and suits

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ let isMinor = false;
 
 // DOM
 // grabbing the template I made and selecting it by attribute
-const cardSkeleton = document.querySelector("[card-skeleton]")
-console.log(cardSkeleton) // shows the doc frag
+const cardSkeleton = document.querySelector("[card-skeleton]");
+console.log(cardSkeleton); // shows the doc frag
 const bodyEl = document.querySelector("body");
 const cardsPanel = document.querySelector("#cards-panel");
 // const searchBtn = document.createElement("button");
@@ -17,10 +17,11 @@ const cardsPanel = document.querySelector("#cards-panel");
 // bodyEl.prepend(searchBtn);
 const searchBar = document.createElement("input");
 searchBar.id = "card-search";
+searchBar.name = "cardSearch";
 searchBar.type = "text";
-searchBar.placeholder = "Search cards...."
+searchBar.placeholder = "Search cards....";
 bodyEl.prepend(searchBar);
-console.log(searchBar)
+console.log(searchBar);
 
 // Data Retrieval
 // baseURL provided by the docs
@@ -44,19 +45,41 @@ async function initialPanel() {
     const data = await response.json();
     // deck targets the array inside my data so I can loop through it more easily
     const deck = data.cards;
-    console.log(data);
+    // console.log(data);
+
     // for each card, do the following (78 cards total - indexed from 0 - 77)
     deck.forEach((card, index) => {
-      const newDiv = document.createElement("div");
-      // setting up a universal class for each card div
-      newDiv.className = "card";
-      // adding their index as an id in case I need to use that for later
-      newDiv.id = index;
-      // for accessibility, repurposing divs to act like list items 
-      newDiv.role = "listitem";
-      // testing data retrieval is successful
-      newDiv.textContent = card.name;
-      cardsPanel.append(newDiv);
+      // setting up template/document fragment cloning for cards
+      // grab everything inside the template (content) and clone it
+      // which returns a document fragment
+      const cardFrag = cardSkeleton.content.cloneNode(true).children[0];
+      const cardName = cardFrag.querySelector("[card-title]");
+      const type = cardFrag.querySelector("[card-type]");
+      const suit = cardFrag.querySelector("[card-suit]");
+      const upright = cardFrag.querySelector("[card-upright]");
+      const reverse = cardFrag.querySelector("[card-reversed]");
+      const desc = cardFrag.querySelector("[card-desc]");
+      cardName.textContent = card.name;
+      type.textContent += card.type;
+      upright.textContent += card.meaning_up;
+      reverse.textContent += card.meaning_rev;
+      desc.textContent += card.desc;
+      // major arcana cards don't have a suit so applying logic to account for that
+      card.suit === undefined
+        ? (suit.textContent += "Not applicable")
+        : (suit.textContent += card.suit);
+      cardsPanel.append(cardFrag);
+
+      // const newDiv = document.createElement("div");
+      // // setting up a universal class for each card div
+      // newDiv.className = "card";
+      // // adding their index as an id in case I need to use that for later
+      // newDiv.id = index;
+      // // for accessibility, repurposing divs to act like list items
+      // newDiv.role = "listitem";
+      // // testing data retrieval is successful
+      // newDiv.textContent = card.name;
+      // cardsPanel.append(newDiv);
     }); // end of forEach loop
   } catch (error) {
     console.error(error.message);
@@ -67,22 +90,29 @@ initialPanel(); // initializes the cards so that there's something on the page w
 
 console.log(cardsPanel);
 const allCards = document.getElementsByClassName("card");
-console.log(allCards) // returns HTML collection
+console.log(allCards); // returns HTML collection
 
 // live searches based on user input
-searchBar.addEventListener("input", liveSearch)
+searchBar.addEventListener("input", liveSearch);
 
 function liveSearch() {
   // the user's input
-    const query = this.value.toLowerCase(); 
-   // for each card inside of the HTML collection (deck) 
-    for (let card of allCards) {
-      // the text content of each card
-        const text = card.textContent.toLowerCase();
-        // if it includes the user search, show it : if not, don't 
-        card.style.display = text.includes(query) ? "block" : "none"
-    }
+  const query = this.value.toLowerCase();
+  // for each card inside of the HTML collection (deck)
+  for (let card of allCards) {
+    // the text content of each card
+    const text = card.textContent.toLowerCase();
+    // if it includes the user search, show it : if not, don't
+    card.style.display = text.includes(query) ? "block" : "none";
+  }
 }
 
+// DOM - Creating the filter
+const label = document.createElement("label");
+label.for = "card-filter";
+const select = document.createElement("select")
+select.id = "card-filter"
+select.name = "filter-cards"
 
-
+const option = document.createElement("option")
+option.value = "Major"
