f2cc0a080dbbd9efbedece4fc9bd0724445e11db
SleepyDroids <jacquelinelwrites@gmail.com>
2025-09-30 00:51:02 -0400

    working on functionality for linking images to each card appropriately

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ let isMinor = false;
 // DOM
 // grabbing the template I made and selecting it by attribute
 const cardSkeleton = document.querySelector("[card-skeleton]");
-console.log(cardSkeleton); // shows the doc frag
+// console.log(cardSkeleton); // shows the doc frag
 const bodyEl = document.querySelector("body");
 const cardsPanel = document.querySelector("#cards-panel");
 // const searchBtn = document.createElement("button");
@@ -67,16 +67,6 @@ async function initialPanel() {
       // major arcana cards don't have a suit so applying logic to account for that
       card.suit === undefined ? suit.remove() : (suit.textContent += card.suit);
       cardsPanel.append(cardFrag);
-      // const newDiv = document.createElement("div");
-      // // setting up a universal class for each card div
-      // newDiv.className = "card";
-      // // adding their index as an id in case I need to use that for later
-      // newDiv.id = index;
-      // // for accessibility, repurposing divs to act like list items
-      // newDiv.role = "listitem";
-      // // testing data retrieval is successful
-      // newDiv.textContent = card.name;
-      // cardsPanel.append(newDiv);
     }); // end of forEach loop
   } catch (error) {
     console.error(error.message);
@@ -115,24 +105,45 @@ const label = document.querySelector("label");
 label.after(select);
 const selectFilter = document.querySelector("#card-filter");
 const optionsData = [
-  { value: "major", text: "Major Arcana" },
-  { value: "minor", text: "Minor Arcana" },
-  { value: "wands", text: "Wands (Suit)" },
-  { value: "cups", text: "Cups (Suit)" },
-  { value: "swords", text: "Swords (Suit)" },
-  { value: "pentacles", text: "Pentacles (Suit)" },
+  { value: "#", text: "Filter cards by type" },
+  { value: "major", text: "Major Arcana", id: "major" },
+  { value: "minor", text: "Minor Arcana", id: "minor" },
+  { value: "wands", text: "Wands (Suit)", id: "wands" },
+  { value: "cups", text: "Cups (Suit)", id: "cups" },
+  { value: "swords", text: "Swords (Suit)", id: "swords" },
+  { value: "pentacles", text: "Pentacles (Suit)", id: "pentacles" },
 ]; // end of optionsData array of objects
 optionsData.forEach((data) => {
   console.log(data);
   const optionCreate = document.createElement("option");
   optionCreate.value = data.value;
   optionCreate.textContent = data.text;
+  optionCreate.id = data.id;
   selectFilter.appendChild(optionCreate);
 });
 
 console.log(selectFilter)
 selectFilter.addEventListener("change", cardFilter)
 
+// can use template to make the cards again for each filter type
+// have the filter link to the card filters from the API docs
+// can make fetch calls per filter though I don't know if that's optimal
+// could try use Promise.all() for each filter type???
+// or if it would be easier just to use the data I've already pulled in my first request
+// but I need to figure out how to prioritze the DOM loading in first 
+// so I have data to iterate over for the filter 
+
 function cardFilter() {
-  
+ // WIP
+}
+
+// separate function here to link images I've downloaded to each card's src accordingly
+// can link cards by their name to the appropriate image
+// alternatively each card has a unique value in the API so if I name the images that way
+// that could work too
+// trying to figure out how to do this without having 78 if statements to account for each card
+// according to stackoverflow I need to use .split() method
+
+function linkImages() {
+
 }
